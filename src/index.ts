b6775064458f8465683ed31d01c52cfa0f5175f1
Erik Huang <ekhu94@gmail.com>
2021-05-08 18:33:29 -0700

    displays google maps correctly passing in params

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello there');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
+
+new google.maps.Map(document.querySelector('#map'), {
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+  zoom: 1,
+  zoomControl: true,
+});
